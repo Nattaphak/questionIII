735ff5f636a29715c09663150d5af039301c7aab
Nattaphak Udomwasurat <nattaphak.udom@kmutt.ac.th>
2022-09-04 18:12:24 +0700

    questionIII

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+        {
+            int power;
+
+            int.TryParse(Console.ReadLine(), out power);
+
+            Console.WriteLine(pow(power));
+
+            Console.ReadLine();
+        }
+
+        public static int pow(int power)
+        {
+            if (power == 0)
+                return 1;
+            return 2*pow(--power);
+        }
+}
